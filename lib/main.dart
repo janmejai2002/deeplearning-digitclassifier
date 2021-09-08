3921fd5bf0ec98e1c397b212ca5729fb5415f946
BuildTools <unconfigured@null.spigotmc.org>
2021-09-08 18:42:29 +0530

    First Commmit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+import 'screens/draw_screen.dart';
+
+void main() => runApp(MaterialApp(
+      home: DrawScreen(),
+    ));
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const mnistImageSize = 28;
+  static const imageSize = 224.0;
+  static const strokeWidth = 5.0;
+  static const canvasSize = 280;
+  static const borderSize = 10.0;
+}
